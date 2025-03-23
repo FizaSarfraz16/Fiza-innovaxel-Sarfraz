eb7857534af1bc7396fab25171da2cc598f3f667
FizaSarfraz16 <fizasarfraz04@gmail.com>
2025-03-23 14:50:09 +0500

    routes are added

---
+++ routes/url.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import {
+  createShortUrl,
+  getUrl,
+  updateShortUrl,
+  deleteShortUrl,
+  getUrlStats
+} from '../controllers/urlController.js';
+
+const router = express.Router();
+
+router.post('/', createShortUrl);
+router.get('/shortCode', getUrl);
+router.put('/shortCode', updateShortUrl);
+router.delete('/:shortCode', deleteShortUrl);
+router.get('/:shortCode/stats', getUrlStats);
+
+export default router;
